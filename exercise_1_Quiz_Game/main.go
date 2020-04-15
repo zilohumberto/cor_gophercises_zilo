938a7a6be5051cf06c6819c87e0665ec42936135
zilohumberto <zilohumberto@gmail.com>
2020-04-15 00:04:34 -0300

    adding multi operation, add args from console

--- exercise_1_Quiz_Game/main.go
+++ exercise_1_Quiz_Game/main.go
@@ -1,81 +1,25 @@
+// exercise come from: https://github.com/gophercises/quiz
 package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"strconv"
-	"strings"
 )
-
-func readLines(c chan []string) {
-	in := `question, result
-3+5,10
-5+4,9
-3+2,6
-`
-	r := csv.NewReader(strings.NewReader(in))
-	_, err := r.Read()
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			close(c)
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		c <- record
-	}
-}
-
-func question(s []string) bool {
-	question := s[0]
-	possibleResult, err := strconv.Atoi(s[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	var operandoLadoA, operandoLadoB, operador string
-	operador = ""
-	for _, _q := range question {
-		q := string(_q)
-		if operador != "" {
-			operandoLadoB += q
-			continue
-		}
-		if q == "+" || q == "-" {
-			operador = q
-			continue
-		}
-		operandoLadoA += q
-	}
-	operandoLadoAInt, err := strconv.Atoi(operandoLadoA)
-	operandoLadoBInt, err := strconv.Atoi(operandoLadoB)
-	calculateResult := calculate(operandoLadoAInt, operandoLadoBInt, operador)
-	return calculateResult == possibleResult
-}
-
-func calculate(a int, b int, o string) int {
-	switch o {
-	case "+":
-		return a + b
-	case "-":
-		return a - b
-	}
-	return 0
-}
 
 func main() {
 	// steps
 	// read a csv file
 	// resolve the problems one problem for line
 	// output the total numbers of problem correct and many problems there were in total
+	filename := readConsole()
+	reader := openFile(filename)
 	c := make(chan []string)
-	go readLines(c)
+	go readLines(reader, c)
 	count, correct := 0, 0
 	var v []string
 	ok := true
@@ -92,4 +36,77 @@ func main() {
 		}
 	}
 	fmt.Println("count", count, "result", correct)
+}
+
+func readConsole() string {
+	filename := flag.String("filename", "problem.csv", "CSV File that conatins quiz questions")
+	flag.Parse()
+	return *filename
+}
+
+func openFile(filename string) io.Reader {
+	reader, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return reader
+}
+
+func readLines(reader io.Reader, c chan []string) {
+	r := csv.NewReader(reader)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			close(c)
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		c <- record
+	}
+}
+
+func question(s []string) bool {
+	questions := s[0]
+	possibleResult, err := strconv.Atoi(s[1])
+	if err != nil {
+		return false
+	}
+	var operandoLadoA, operandoLadoB, operador string
+	operador = ""
+	for _, _question := range questions {
+		q := string(_question)
+		if operador != "" {
+			operandoLadoB += q
+			continue
+		}
+		if q == "+" || q == "-" || q == "*" {
+			operador = q
+			continue
+		}
+		operandoLadoA += q
+	}
+	operandoLadoAInt, err := strconv.Atoi(operandoLadoA)
+	if err != nil {
+		return false
+	}
+	operandoLadoBInt, err := strconv.Atoi(operandoLadoB)
+	if err != nil {
+		return false
+	}
+	calculateResult := calculate(operandoLadoAInt, operandoLadoBInt, operador)
+	return calculateResult == possibleResult
+}
+
+func calculate(a int, b int, o string) int {
+	switch o {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	}
+	return 0
 }
