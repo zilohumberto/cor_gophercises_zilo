d05cdf97740a7d9888aff8251a021cef8aa7635b
zilohumberto <zilohumberto@gmail.com>
2020-04-16 15:22:17 -0300

    step 2. read yaml from os

--- exercise_2_URL_shortener/main.go
+++ exercise_2_URL_shortener/main.go
@@ -2,13 +2,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/zilohumberto/cor_gophercises_zilo/exercise_2_URL_shortener/urlshort"
 )
 
+var (
+	yamlFile = flag.String("input", "example.yml", "yaml example urls")
+)
+
 func main() {
+	flag.Parse()
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -20,18 +28,23 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
+	yaml := readFile()
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFile() string {
+	data, err := ioutil.ReadFile(*yamlFile)
+	check(err)
+	return string(data)
 }
 
 func defaultMux() *http.ServeMux {
